13c23df0954beb3432b9620a1a340e6b46b04601
mh-swift <zuojiadazhongma@gmail.com>
2024-01-03 13:48:06 +0800

    init

---
+++ source/001-环境.py
@@ -0,0 +1,31 @@
+# 安装
+# 官网地址: https://www.python.org
+# 下载地址: https://www.python.org/downloads/
+# 文档地址: https://docs.python.org/zh-cn/3/
+
+# 下载最新的对应操作系统的版本:
+# 1. win安装详细文档:https://docs.python.org/zh-cn/3/using/windows.html#windows-full
+# 2. mac安装详细文档:https://docs.python.org/zh-cn/3/using/mac.html
+
+# 注意: 需要把python可执行文件添加到path下,方便全局使用.
+
+# 安装完成后,重启终端，输入`python --version`或者`python3 --version`检测是否安装成功.
+
+
+# IDE
+# 1. visual studio code下载地址: https://code.visualstudio.com/
+
+# IDE插件
+# 1. Python语法支持:Python
+# 2. Python语法分析:Flake8
+# 3. Python代码格式化:Black Formatter
+
+# IDE settings.json Python相关配置
+"""
+"[python]": {
+    "editor.defaultFormatter": "ms-python.black-formatter",
+    "editor.formatOnSave": true,
+    "editor.tabSize": 4
+}
+"""
+print("Python开发环境配置成功")
