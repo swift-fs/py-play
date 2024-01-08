90b5f90e6db7a262a088efc20caab1f0cc9779d9
mh-swift <zuojiadazhongma@gmail.com>
2024-01-08 19:19:41 +0800

    feat

--- src/oop/004-自定义操作符.py
+++ src/oop/004-自定义操作符.py
@@ -15,9 +15,29 @@ class MyDate:
     def __repr__(self) -> str:
         return f"MyDate:{self.year}-{self.month}-{self.day}"
 
+    # __eq__() 方法用于比较两个对象是否相等。（实现对象的 == 比较）
+    def __eq__(self, __value: object) -> bool:
+        print("eq is called")
+        if not isinstance(__value, MyDate):
+            return False
+        return (
+            self.year == __value.year
+            and self.month == __value.month
+            and self.day == __value.day
+            and hash(self) == hash(__value)
+        )
+
     def __hash__(self) -> int:
         print(f"{self} -> hash called")
         return hash((self.year, self.month, self.day))
+
+    def __bool__(self):
+        print("bool is called")
+        return self.year > 2021
+
+    # 对象不被使用时会调用
+    def __del__(self):
+        print("del is called")
 
 
 def main():
@@ -26,6 +46,8 @@ def main():
     print(my_date01)
     # 调用对象的__repr__方法
     print(repr(my_date01))
+    # 调用对象的__del__方法
+    del my_date01
 
     my_date02 = MyDate(2023, 1, 1)
 
@@ -33,10 +55,13 @@ def main():
     my_date03 = MyDate(2023, 1, 1)
     print(my_date03)
     print(my_date02 is my_date03)
+    # 调用对象的__eq__方法
     print(my_date02 == my_date03)
-
+    # 调用对象的__hash__方法
     print(hash(my_date02))
     print(hash(my_date03))
+    # 调用对象的__bool__方法
+    print(bool(my_date03))
 
 
 if __name__ == "__main__":
