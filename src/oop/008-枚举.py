57f34d27b32df845442d45dfe5a6f16f70fe1234
mh-swift <zuojiadazhongma@gmail.com>
2024-01-09 18:47:58 +0800

    fix

--- src/oop/008-枚举.py
+++ src/oop/008-枚举.py
@@ -15,6 +15,22 @@ class Status(Enum):
     # 会报错，上面200已被使用
     # OK = 200
 
+    # 自定义 == 比较
+    def __eq__(self, __value: object) -> bool:
+        print("status __eq__ called")
+        if isinstance(__value, int):
+            return self.value == __value
+        if isinstance(__value, str):
+            return self.name == str.upper()
+
+        if isinstance(__value, type(self)):
+            return self.value == __value.value
+
+    def __lt__(self, __value: object) -> bool:
+        print("__lt__ called")
+
+        return False
+
 
 class Student:
     def __init__(self, name, gender: Gender):
@@ -46,6 +62,10 @@ def main():
 
     for k, v in Status.__members__.items():
         print(f"{k} -> {v.value}")
+
+    print(Status.SUCCESS == 200)
+    print(Status.SUCCESS == Status.SUCCESS)
+    print(Status.SUCCESS < Status.FAIL)
 
 
 if __name__ == "__main__":
---
+++ src/oop/009-描述符.py
@@ -0,0 +1,143 @@
+# descriptor
+# 挺重要的概念,很多开源库都会使用到
+# 描述符的定义很简单，实现了下列任意一个方法的 Python 对象就是一个描述符（descriptor）：
+# __get__(self, obj, type=None)
+# __set__(self, obj, value)
+# __delete__(self, obj)
+
+# self 是当前定义的描述符对象实例。
+# obj 是该描述符将作用的对象实例。
+# type 是该描述符作用的对象的类型（即所属的类）。
+
+# 描述器让对象能够自定义属性查找、存储和删除的操作。
+
+from pathlib import Path
+
+
+# 返回常量的描述器
+class Ten:
+    def __get__(self, obj, objtype=None):
+        print("get called")
+        print(obj)
+        print(objtype)
+        return 10
+
+
+# 动态查找
+class DirectorySize:
+    def __get__(self, obj, objtype=None):
+        p = Path(__file__).parent.parent.parent
+        p: Path = p / obj.dirname
+        return len(list(p.glob("*")))
+
+
+class Directory:
+    size = DirectorySize()
+
+    def __init__(self, dirname: str) -> None:
+        self.dirname = dirname
+
+
+class RequireAge:
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            raise AttributeError("can't get age from class")
+        value = obj._age
+        return value
+
+    def __set__(self, obj, value):
+        print("设置年龄:", value)
+        if value < 0:
+            raise ValueError(f"invalid age {value}")
+        obj._age = value
+
+    # 当一个类使用描述器时，它可以告知每个描述器使用了什么变量名。
+    def __set_name__(self, owner, name):
+        print("属性名字:", name)
+        self.name = name
+
+
+class Person:
+    age = RequireAge()
+
+    def __init__(self, name, age=1):
+        self.name = name
+        self.age = age  # Calls __set__()
+
+    def increase(self):
+        self.age += 1
+
+
+# 要使用描述器，它必须作为一个类变量存储在另一个类中
+class Square:
+    x = Ten()
+    y = Ten()
+    z = 5
+
+
+# 完整例子,完全动态托管属性
+
+
+class RequireStr:
+    def __set_name__(self, owner, name):
+        self.name = f"_{name}"
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.name)
+
+    def __set__(self, obj, value):
+        setattr(obj, self.name, value)
+
+
+class Student:
+    name = RequireStr()
+    gender = RequireStr()
+
+    def __init__(self, name, gender) -> None:
+        self.name = name
+        self.gender = gender
+
+
+def main():
+    # 入门
+    square = Square()
+    print(square.x)
+    print(square.y)
+    print(square.z)
+    print(Square.__dict__)
+
+    # 动态获取指定文件夹下的子文件数量
+    print(Directory("src/oop").size)
+    print(Directory("src").size)
+
+    # 托管属性 -> 托管对实例数据的访问
+    # 描述器被分配给类字典中的公开属性，而实际数据作为私有属性存储在实例字典中。
+    # 当访问公开属性时，会触发描述器的 __get__() 和 __set__() 方法。
+    p = Person("swift")
+    p.age = 10
+    p.increase()
+    print(p.age)
+
+    p1 = Person("echo")
+    p1.age = 19
+    p1.increase()
+    print(p1.age)
+
+    # 互不影响，都存在自己的实例字典中
+    print(f"p age: {p.age}")
+    print(f"p1 age: {p1.age}")
+    print(p.__dict__)
+    print(p1.__dict__)
+
+    # 报错
+    # print(Person.age)
+
+    # 完整的例子
+    student = Student(name="swift", gender="male")
+    student01 = Student(name="echo", gender="female")
+    print(f"student: {student.name}, {student.gender}")
+    print(f"student01: {student01.name}, {student01.gender}")
+
+
+if __name__ == "__main__":
+    main()
