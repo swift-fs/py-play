3d433c1ec3799ab8fcc44d28a9a694291707c1fb
swift <zuojiadazhongma@gmail.com>
2024-01-11 21:22:23 +0800

    feat

--- src/高级/004-typeclass.py
+++ src/高级/004-typeclass.py
@@ -1,37 +1,29 @@
+# 任何class在内存里就是一个type类的对象
+# python使用type类来创建其他的class
+# 语法: type(class_name,parents,class_dict)
+# 理论上来讲，可以使用type来动态创建class
+
+from pathlib import Path
+
+
 class Student:
     def greeting(self):
-        print("Hello student")
+        print("hello student")
 
 
-print(Student)
-print(type(Student))
-
-print(isinstance(Student, type))
-
-
-class_body = """
-def greeting(self):
-    print("Hello customer")
-def jump(self):
-    print("jump")
-"""
-class_dict = {}
-
-
-exec(class_body, globals(), class_dict)
-print(class_dict)
-# 动态定义类
-Customer = type("Customer", (object,), class_dict)
-print(Customer)
-c = Customer()
-c.greeting()
-c.jump()
+def main():
+    print(type(Student))
+    print(isinstance(Student, type))
+    # 动态内容，可以通过其他加载过来，比如用户上传的文本文件
+    dynamic_content = Path(__file__).parent / "dynamic_content.txt"
+    class_body = dynamic_content.read_text(encoding="utf-8")
+    class_dict = {}
+    # 可以把一个python语法的字符串内容转换为一个字典
+    exec(class_body, globals(), class_dict)
+    Customer = type("Customer", (object,), class_dict)
+    customer = Customer()
+    customer.greeting()
 
 
-def hi():
-    a = 10
-    b = 20
-    print(locals())
-
-
-hi()
+if __name__ == "__main__":
+    main()
